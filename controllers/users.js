fe463c57171b59184f4b70752e8060eb9bb83bde
emachipana <emachipana0102@gmail.com>
2022-12-06 12:27:11 -0500

    ADD: get users friends on users controller

--- controllers/users.js
+++ controllers/users.js
@@ -12,3 +12,26 @@ export const getUser = async (req, res) => {
     res.status(404).json({ error: err.message });
   }
 }
+
+export const getUserFriends = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if(!user) return res.status(404).json({ message: "User does not exist." });
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+
+    const formattedFriends = friends.map((friend) => {
+      const {friends, ...user} = friend.toJSON();
+      return user;
+    });
+
+    res.json(formattedFriends);
+  }catch(err) {
+    res.status(404).json({ error: err.message });
+  }
+}
+
+export const addRemoveFriend = () => "";
