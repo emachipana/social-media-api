021a7a8de364a9700e621cc5707785e6cc4c4153
emachipana <emachipana0102@gmail.com>
2022-12-06 13:11:37 -0500

    ADD: add or remove friend on users controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+import formatFriends from "../helpers/formatFriends.js";
 import User from "../models/User.js";
 
 // GET || READ
@@ -19,14 +20,7 @@ export const getUserFriends = async (req, res) => {
     const user = await User.findById(id);
     if(!user) return res.status(404).json({ message: "User does not exist." });
 
-    const friends = await Promise.all(
-      user.friends.map((id) => User.findById(id))
-    );
-
-    const formattedFriends = friends.map((friend) => {
-      const {friends, ...user} = friend.toJSON();
-      return user;
-    });
+    const formattedFriends = await formatFriends(User, user);
 
     res.json(formattedFriends);
   }catch(err) {
@@ -34,4 +28,30 @@ export const getUserFriends = async (req, res) => {
   }
 }
 
-export const addRemoveFriend = () => "";
+// PATCH || UPDATE
+export const addRemoveFriend = async (req, res) => {
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
+    if(!user || !friend) return res.status(404).json({ message: "User does not exist." });
+
+    if(user.friends.includes(friendId)) {
+      // remove friend
+      user.friends = user.friends.filter((id) => id !== friend);
+      friend.friends = friend.friends.filter((userId) => userId !== id);
+    }else {
+      // add friend
+      user.friends.push(friendId);
+      friend.friends.push(id);
+    }
+    await user.save();
+    await friend.save();
+
+    const formattedFriends = await formatFriends(User, user);
+
+    res.json(formattedFriends);
+  }catch(err) {
+    res.status(500).json({ error: err.message });
+  }
+}
