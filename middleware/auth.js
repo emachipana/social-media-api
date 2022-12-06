161283dfea5e140966f0be0317f56896c2c61296
emachipana <emachipana0102@gmail.com>
2022-12-06 10:17:33 -0500

    ADD: middleware to verify token

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = async (req, res, next) => {
+  try {
+    let token = req.header("Authorization");
+
+    if(!token) return res.status(402).json({ message: "Access Denied" });
+
+    if(token.startsWith("Bearer ")) {
+      token = token.split(" ")[1];
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    
+    next();
+  }catch(err) {
+    res.status(500).json({ error: err.message });
+  }
+}
