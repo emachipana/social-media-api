5a7344089e4de3aff1af262bb47ecc557081926f
emachipana <emachipana0102@gmail.com>
2022-12-06 19:56:53 -0500

    ADD: like a post route

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 import { verifyToken } from "../middleware/auth.js";
 import {
   getFeedPosts,
-  getUserPosts
+  getUserPosts,
+  likePost
 } from "../controllers/posts.js";
 
 const router = Router();
@@ -10,5 +11,8 @@ const router = Router();
 // GET || READ
 router.get("/", verifyToken, getFeedPosts);
 router.get("/:userId/posts", verifyToken, getUserPosts);
+
+// PATCH || UPDATE
+router.patch("/:id/like", verifyToken, likePost);
 
 export default router;
