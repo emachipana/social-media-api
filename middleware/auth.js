cb16125bf111cefc41af52666602f74b815d5862
emachipana <emachipana0102@gmail.com>
2022-12-06 20:22:14 -0500

    FIX: change status code to 401

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ export const verifyToken = async (req, res, next) => {
   try {
     let token = req.header("Authorization");
 
-    if(!token) return res.status(402).json({ message: "Access Denied" });
+    if(!token) return res.status(401).json({ message: "Access Denied" });
 
     if(token.startsWith("Bearer ")) {
       token = token.split(" ")[1];
