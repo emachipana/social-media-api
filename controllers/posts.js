0f6b1c84242cfe3302d26c58144390ab1b99aa83
emachipana <emachipana0102@gmail.com>
2022-12-07 09:04:52 -0500

    ADD: likea a post on posts controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -30,6 +30,7 @@ export const createPost = async (req, res) => {
 export const getFeedPosts = async (_req, res) => {
   try {
     const posts = await Post.find();
+
     res.json(posts);
   }catch(err) {
     res.status(500).json({ error: err.message });
@@ -40,10 +41,38 @@ export const getUserPosts = async (req, res) => {
   try {
     const { userId } = req.params;
     const posts = await Post.find({ userId });
+
     res.json(posts);
   }catch(err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ error: err.message });
   }
 }
 
-// export const likePost = () => "";
+// PATCH || UPDATE
+export const likePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await Post.findById(id);
+    if(!post) return res.status(404).json({ message: "Post not found" });
+
+    const isLiked = post.likes.get(userId);
+    if(isLiked) {
+      // remove like
+      post.likes.delete(userId);
+    }else {
+      // add like
+      post.likes.set(userId, true);
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { likes: post.likes },
+      { new: true }
+    );
+
+    res.json(updatedPost);
+  }catch(err) {
+    res.status(404).json({ error: err.message });
+  }
+}
